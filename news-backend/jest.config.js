18a8140cc7b4c40ba0c6c6822c19773f3a123fe9
Shrey Shukla <shreyshukla318@gmail.com>
2025-05-01 23:26:31 +0530

    Simplify backend test configuration

--- news-backend/jest.config.js
+++ news-backend/jest.config.js
@@ -3,10 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   passWithNoTests: true,
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   coveragePathIgnorePatterns: ['/node_modules/'],
-  collectCoverageFrom: ['**/*.js', '!**/node_modules/**'],
-  transform: {},
-  moduleNameMapper: {
-    '^(\.{1,2}/.*)\.js$': '$1'
-  }
+  collectCoverageFrom: ['**/*.js', '!**/node_modules/**']
 };
